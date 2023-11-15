f9e647945f4629737d4f8dc5887bc9a71cdbd2ce
echocslsk <CardioLeo@gmail.com>
2023-11-15 00:24:18 -0700

    after code was put in scratch pad, this commit removes that same code from the index.js doc, and also comments out several lines that stayed in the index.js

--- take3/index.js
+++ take3/index.js
@@ -25,50 +25,15 @@
 
 class Menu {
 	constructor() {
-		let player1 = new Player("Joeski", [0], 0);
-		console.log(player1);
-		let player2 = new Player("Broski", [0], 0);
-		console.log(player2);
+	//	let player1 = new Player("Joeski", [0], 0);
+	//	console.log(player1);
+	//	let player2 = new Player("Broski", [0], 0);
+	//	console.log(player2);
 	}
 
 	start_game() {
-		
+		alert("war card game started!") // alert works
 	}
-}
-
-class Deck {
-	let rank_values = {
-		"2": 2,
-		"3": 3,
-		"4": 4,
-		"5": 5,
-		"6": 6,
-		"7": 7,
-		"8": 8,
-                "9": 9,
-                "10": 10,
-                "J": 11,
-                "Q": 12,
-                "K": 13,
-                "A": 14
-	}
-	console.log(rank_values)
-	let suites_list = ["Hearts", "Spades", "Diamonds", "Clubs"]
-	console.log(suites_list);
-}
-
-class Card(rank, suite, value) {
-	constructor(rank, suite, value) {
-		let this.rank = rank;
-		let this.suite = suite;
-		let this.value = value;
-	}
-}
-
-class Player(name, hand, score) {
-	let this.name = "";
-	let this.hand = [];
-	let this.score = 0;
 }
 
 let menu = new Menu();
