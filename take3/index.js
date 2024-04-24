56820ada44aa4a1e6aa760593bf5e4910255d4bb
echocslsk <CardioLeo@gmail.com>
2024-04-23 19:59:23 -0600

    no time for in-depth commit message due to kids; but this is a major update in functionality

--- take3/index.js
+++ take3/index.js
@@ -9,32 +9,135 @@
 
 // Game Logic:
 // 1. Define Cards/Players/Decks(/hands?)
-// 2. Deal 26 cards to each player from one deck
-// 3. Highest card in each round gets each player an extra point
+// -- -- -- Decks, Players, Menu defined;
+// -- -- -- Means of printing values defined;
+// -- -- -- Sleep function defined;
+// 2. - -- Define shuffle algorithm & function
+// 3. Deal 26 cards to each player from one deck
+// 4. Highest card in each round gets each player an extra point
 // -- ties result in zero points for both players
-// 4. After all cards play, display score, declare winner
+// 5. After all cards play, display score, declare winner
 
 // -----
 
 // Player Has Hand, Hand has Cards;
-// Card has Emblem, Number (K,Q,J);
+// Card has Emblem or Number (K,Q,J);
 // Card is an instance of an Object belonging to Hand;
-// Hand is an instance of an Object belonging to Deck;
+// Hand is an instance of an Object belonging to Deck; // Is this true?
+// 							/ I don't think so
 
 // Need Shuffle Logic.
 
-class Menu {
-	constructor() {
-	//	let player1 = new Player("Joeski", [0], 0);
-	//	console.log(player1[0]);
-	//	let player2 = new Player("Broski", [0], 0);
-	//	console.log(player2);
-	}
+//
+//
+//
 
-	start_game() {
-		alert("war card game started!") // alert works
+function sleep(ms) { 
+        return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function print_all_in(this_set) {
+	alert('printing all elements in set');
+	for (let element in this_set){
+		sleep(750).then(() => { console.log(element); });
+                                // 750 is a good arg to pass to sleep
+				// if doing console.log();
+				// but for alert, even 50 feels too large
 	}
 }
 
+class Menu {
+        constructor() {
+        //      let player1 = new Player("Joeski", [0], 0);
+        //      console.log(player1[0]);
+        //      let player2 = new Player("Broski", [0], 0);
+        //      console.log(player2);
+        }
+
+        start_game() {
+                alert("war card game started!") // alert works
+        }
+}
+
+class Deck {
+        
+	// data members
+
+	ranks = {
+                "2": 1,
+                "3": 2,
+                "4": 3,
+                "5": 4,
+                "6": 5,
+                "7": 6,
+                "8": 7,
+                "9": 8,
+                "10": 9,
+                "J": 10,
+                "Q": 11,
+                "K": 12,
+                "A": 13,
+	};
+
+        suites = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        
+	// methods
+
+	print_suites() {
+		alert("printing suites");
+                for (let i = 0; i < this.suites.length; i++){
+                        // alert(suites[i]);
+			console.log(this.suites[i]); 
+                }
+        }
+}	
+
+class Card {
+        constructor(rank, suite, points) {
+                this.rank = rank;
+                this.suite = suite;
+                this.points = points;
+        }
+
+	make_52_cards(deck_suites, deck_ranks){
+		for (let suite_num = 0; suite_num < deck_suites.length; suite_num++){
+			console.log("Suite: ", deck_suites[suite_num]);
+			for (let element in deck_ranks){
+				console.log("Rank: ", element);
+			}
+			// for (let rank_num = 0; rank_num < deck_ranks.length; rank_num++){
+			// 	console.log("Rank: ", rank_num);
+			// }
+		}
+	}
+}
+
+class Player { // add name, hand, score as parameters to constructor?
+        name = "";
+        // hand = [];
+        score = 0;
+
+	constructor(name, score){
+		this.name = name;
+		// this.hand = hand;
+		this.score = score;
+	}
+}
+
+//
+
 let menu = new Menu();
+let deck = new Deck();
+let player1 = new Player("Curly", 0);
+let player2 = new Player("Moe", 0);
+
+let card = new Card(deck.suites[0], deck.suites[1], deck.suites[2]);
+console.log(card.rank, card.suite, card.points);
+card.make_52_cards(deck.suites, deck.ranks);
+
 menu.start_game();
+print_all_in(deck.ranks);
+deck.print_suites();
+console.log(player1);
+console.log(player2);
+print_all_in(deck.suites); // only prints indices and not strings of suites
--- take3/scratch_pad.js
+++ take3/scratch_pad.js
@@ -2,25 +2,58 @@
 //
 //
 
-class Deck {
-        let rank_values = {
-                "2": 2,
-                "3": 3,
-                "4": 4,
-                "5": 5,
-                "6": 6,
-                "7": 7,
-                "8": 8,
-                "9": 9,
-                "10": 10,
-                "J": 11,
-                "Q": 12,
-                "K": 13,
-                "A": 14
+class Menu {
+        constructor() {
+        //      let player1 = new Player("Joeski", [0], 0);
+        //      console.log(player1[0]);
+        //      let player2 = new Player("Broski", [0], 0);
+        //      console.log(player2);
         }
-        console.log(rank_values)
-        let suites_list = ["Hearts", "Spades", "Diamonds", "Clubs"]
-        console.log(suites_list);
+
+        start_game() {
+                alert("war card game started!") // alert works
+        }
+}
+
+class Deck {
+        
+	// data members
+
+	let ranks = {
+                "2": 1,
+                "3": 2,
+                "4": 3,
+                "5": 4,
+                "6": 5,
+                "7": 6,
+                "8": 7,
+                "9": 8,
+                "10": 9,
+                "J": 10,
+                "Q": 11,
+                "K": 12,
+                "A": 13
+        };
+
+        let suites = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        
+	// methods
+
+	print_ranks(this.ranks){
+		for (i = 0; i >= this.ranks.length-1; i++){
+			console.log(i);
+		}
+	}
+        
+	print_suites(this.suites){
+                for (i = 0; i >= this.suites.length-1; i++){
+                        console.log(i); 
+                }
+        }
+
+	// make_52(){
+		// make it call the card constructor 52 times, etc?
+	// }
 }
 
 class Card(rank, suite, value) {
@@ -37,3 +70,13 @@ class Player(name, hand, score) {
         let this.score = 0;
 }
 
+let menu = new Menu();
+let deck = new Deck();
+let player1 = new Player("Curly");
+let player2 = new Player("Moe");
+
+menu.start_game();
+deck.print_ranks();
+deck.print_suites();
+console.log(player1.name);
+console.log(player2.name);
